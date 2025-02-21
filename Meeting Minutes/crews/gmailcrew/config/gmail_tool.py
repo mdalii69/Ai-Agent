b7e4ac9a386b3b0014a482e51ef2146d67f60f8f
Mohammad Ali <134193466+mdalii69@users.noreply.github.com>
2025-02-21 20:09:33 +0530

    Add files via upload

---
+++ Meeting Minutes/crews/gmailcrew/config/gmail_tool.py
@@ -0,0 +1,39 @@
+
+from typing import Type
+
+from crewai.tools import BaseTool
+from pydantic import BaseModel, Field
+
+from .gmail_utility import authenticate_gmail, create_message, create_draft
+from agentops import record_tool
+
+import os
+
+class GmailToolInput(BaseModel):
+    """Input schema for GmailTool."""
+
+    body: str = Field(..., description="The body of the email to send.")
+
+@record_tool("This is used for gmail draft emails.")
+class GmailTool(BaseTool):
+    name: str = "GmailTool"
+    description: str = (
+        "Clear description for what this tool is useful for, you agent will need this information to use it."
+    )
+    args_schema: Type[BaseModel] = GmailToolInput
+
+    def _run(self, body: str) -> str:
+        try:
+            service = authenticate_gmail()
+
+            sender ="alihussain999111@gmail.com"
+            to = "mdali.me000@gmail.com"
+            subject = "Meeting Minutes"
+            message_text = body
+
+            message = create_message(sender, to, subject, message_text)
+            draft = create_draft(service, "me", message)
+
+            return f"Email sent successfully! Draft id: {draft['id']}"
+        except Exception as e:
+            return f"Error sending email: {e}"
